1166906917528f64f80d0c948372d54f0f86898c
steph van den Beld <semfuchs211@gmail.com>
2020-11-19 19:31:53 +0100

    github yes

--- dbos.api.js
+++ dbos.api.js
@@ -1,23 +1,42 @@
-const DA_pack = require('./package.json');
+const pkg = require('./package.json');
 const updateNotifier = require('update-notifier');
+const https = require('https');
 const request = require('request');
+const fetch = require('node-fetch');
 var modulename = "DBOS.api";
 
-const notifier = updateNotifier({pkg});
- 
-notifier.notify();
- 
-console.log(notifier.update);
+const API = "https://dbos.glitch.me/api";
 
-const API = "https://dbos.glitch.me";
 
+updateNotifier({
+	pkg: {
+		name: pkg.name,
+		version: pkg.version
+	},
+	updateCheckInterval: 0
+}).notify();
 module.exports = {
+    get: {
+        user: {
+            _info: async function(userId, serverId){
+                if(!userId){
+                    new TypeError('No userId provided!');
+                }
+                if(!serverId){
+                    var target = "/global";
+                } else {
+                    var target = "/s/" + serverId;
+                }
+                let settings = { method: "Get" };
+                const res = await fetch(API + target + '/u/' + userId, settings)
+                const proJson = await res.json();
+                // console.log(proJson.user)
+                return proJson.user;
+            }
+        },
+        guild: {
 
-    user: {
-       
-    },
-    guild: {
-
+        }
     }
 
 };
