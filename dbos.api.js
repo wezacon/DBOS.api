4b4fe76d86d9ad86e3ca2f3075f63f2653acdeab
steph van den Beld <semfuchs211@gmail.com>
2020-11-19 16:41:56 +0100

    e

---
+++ dbos.api.js
@@ -0,0 +1,16 @@
+const DA_pack = require('./package.json');
+const request = require('request');
+var modulename = "DBOS.api";
+
+const API = "https://service.narvii.com/api/v1";
+
+module.exports = {
+
+    user: {
+       
+    },
+    guild: {
+
+    }
+    
+};
